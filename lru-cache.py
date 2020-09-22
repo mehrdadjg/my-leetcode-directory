1ad7d34255a79deee655cd537a8b790fd4fba384
Mehrdad Jafarigiv <mehrdadjg@gmail.com>
2020-09-21 22:39:52 -0600

    Added submission results and description tests.

--- lru-cache.py
+++ lru-cache.py
@@ -1,3 +1,8 @@
+""" As of 09/21/2020 21:17 MST
+    Runtime: 248 ms (beats 38.91% of python3 submissions)
+    Memory: 23 MB (beats 67.67% of python3 submissions)
+    Overall: Not impressive buddy...
+"""
 from Library.DoublyConnectedLinkedList import DoublyConnectedLinkedList as LinkedList
 
 
@@ -16,7 +21,7 @@ class LRUCache:
             n_value = self.storage[key]
             self.usage.moveToTail(n_value)
 
-            return n_value.getValue()
+            return n_value.getValue()[self.VALUE]
         else:
             return -1
 
@@ -39,9 +44,14 @@ class LRUCache:
         return self.usage.__repr__()
 
 
-lru = LRUCache(1)
-print(lru.put(2, 1))
-print(lru.get(2))
-print(lru.put(3, 2))
-print(lru.get(3))
-print(lru.get(1))
+if __name__ == '__main__':
+    lru = LRUCache(2)
+    lru.put(1, 1)
+    lru.put(2, 2)
+    print(lru.get(1))
+    lru.put(3, 3)
+    print(lru.get(2))
+    lru.put(4, 4)
+    print(lru.get(1))
+    print(lru.get(3))
+    print(lru.get(4))
